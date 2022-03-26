3f69d8ed943430419b298ab9cf805bba4696fd63
engineerStuardo <ingeniero.stuardo@gmail.com>
2022-03-26 12:06:46 -0600

    moving counter screen to its own component and add RecoilRoot to the app to wrap all the components or screens

---
+++ src/screens/Counter.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+
+export const Counter = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter</Text>
+      <Text style={styles.number}>0</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.addNumberText}>Add number</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  number: {
+    fontSize: 19,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 15,
+    borderRadius: 20,
+  },
+  addNumberText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '900',
+  },
+});
