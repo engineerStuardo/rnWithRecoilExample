ccea068c8b832b098b351bf8f80a3dd3259447c6
engineerStuardo <ingeniero.stuardo@gmail.com>
2022-03-26 12:32:11 -0600

    building basic counter app using recoil as state management

--- src/screens/Counter.tsx
+++ src/screens/Counter.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {useRecoilState} from 'recoil';
+
+import {counterState} from '../recoil/counterAtom';
 
 export const Counter = () => {
+  const [counter, setCounter] = useRecoilState(counterState);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Counter</Text>
-      <Text style={styles.number}>0</Text>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.addNumberText}>Add number</Text>
-      </TouchableOpacity>
+      <Text style={styles.number}>{counter}</Text>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity
+          style={styles.buttonDecrease}
+          onPress={() => {
+            setCounter(counter - 1);
+          }}>
+          <Text style={styles.addNumberText}>Decrease</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonIncrease}
+          onPress={() => {
+            setCounter(counter + 1);
+          }}>
+          <Text style={styles.addNumberText}>Increase</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -28,10 +46,23 @@ const styles = StyleSheet.create({
     fontSize: 19,
     marginBottom: 10,
   },
-  button: {
-    backgroundColor: 'blue',
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonDecrease: {
+    backgroundColor: 'red',
     padding: 15,
     borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 8,
+  },
+  buttonIncrease: {
+    backgroundColor: 'green',
+    padding: 15,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   addNumberText: {
     color: 'white',
